07f515228c48da1c16a2efe03733ebff748cdf2d
gunelbalayeva <gunelbalayeva97@gmail.com>
2025-10-05 10:17:34 +0400

    Initial Commit

---
+++ WeatherApp_SwiftUI/WeatherApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftUIApp.swift
+//  WeatherApp_SwiftUI
+//
+//  Created by User on 05.10.25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
